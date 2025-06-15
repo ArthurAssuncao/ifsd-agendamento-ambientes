792d36e052340758779b2e706aca59f37e385ca9
arthurassuncao <arthur.xx92@gmail.com>
2025-06-15 20:05:53 -0300

    fix: viewport in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br" className="light" data-theme="light">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=0"
-        />
-      </Head>
+      <Head></Head>
       <body className="antialiased">
         <Main />
         <NextScript />
